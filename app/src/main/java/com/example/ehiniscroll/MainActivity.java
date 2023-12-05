7329c93ad742b16edaa83690cefcd5133042d5aa
Stingray <stingraysonggay@gmail.com>
2023-12-05 18:24:23 +0800

    Link button creation

--- app/src/main/java/com/example/ehiniscroll/MainActivity.java
+++ app/src/main/java/com/example/ehiniscroll/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout.LayoutParams;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,8 +21,48 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 showToast("Button Clicked!");
+
+                if (atMiddleOfScreen(myButton)) {
+                    changeButtonPropertiesToLinked(myButton);
+                    changeButtonPositionToTopLeft(myButton);
+                } else {
+                    setContentView(R.layout.activity_main);
+                }
             }
         });
+    }
+
+    private void changeButtonPropertiesToLinked(Button button) {
+        button.setText("Click To Stop link");
+
+        LayoutParams params = new LayoutParams(300, 200);
+        button.setLayoutParams(params);
+    }
+
+    private void changeButtonPositionToTopLeft(Button button) {
+        LayoutParams params = (LayoutParams) button.getLayoutParams();
+
+        params.setMargins(50, 100, 0, 0);
+        button.setLayoutParams(params);
+    }
+
+    private Boolean atMiddleOfScreen(Button button) {
+        String buttonText = button.getText().toString();
+
+        if (buttonText.equals("Click To Start Link")) {
+           return Boolean.TRUE;
+        }
+
+        return Boolean.FALSE;
+    }
+
+    private void changeButtonPropertiesToUnlinked(Button button) {
+        button.setText("Click To Start Link");
+
+        LayoutParams params = new LayoutParams(200, 150);
+        params.setMargins(0, 16, 0, 0);
+
+        button.setLayoutParams(params);
     }
 
     private void showToast(String message) {
