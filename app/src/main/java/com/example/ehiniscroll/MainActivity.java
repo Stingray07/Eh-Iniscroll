6b1bcf237850448f9a3ee0058db510f08c1bb381
Stingray <stingraysonggay@gmail.com>
2023-12-06 20:45:05 +0800

    Trying to find a way to center button

--- app/src/main/java/com/example/ehiniscroll/MainActivity.java
+++ app/src/main/java/com/example/ehiniscroll/MainActivity.java
@@ -1,14 +1,23 @@
 package com.example.ehiniscroll;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout.LayoutParams;
+import android.widget.RelativeLayout;
 import android.widget.Toast;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "ButtonInfo";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,17 +25,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Button myButton = findViewById(R.id.myButton);
+        Map<String, Integer> unlinkedButtonInfo = getButtonInfo(myButton);
+        for (Map.Entry<String, Integer> entry : unlinkedButtonInfo.entrySet()) {
+            String key = entry.getKey();
+            int value = entry.getValue();
+            System.out.println(key + ": " + value);
+        }
+
 
         myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 showToast("Button Clicked!");
+                Map<String, Integer> currentButtonInfo = getButtonInfo(myButton);
+                currentButtonInfo.put("yPos", 0);
+                currentButtonInfo.put("xPos", 0);
 
-                if (atMiddleOfScreen(myButton)) {
+                for (Map.Entry<String, Integer> entry : currentButtonInfo.entrySet()) {
+                    String key = entry.getKey();
+                    int value = entry.getValue();
+                    System.out.println(key + ": " + value);
+                }
+
+                if (currentButtonInfo.equals(unlinkedButtonInfo)) {
+                    System.out.println("I was here");
                     changeButtonPropertiesToLinked(myButton);
                     changeButtonPositionToTopLeft(myButton);
                 } else {
-                    setContentView(R.layout.activity_main);
+                    setButtonProperties(myButton, unlinkedButtonInfo);
                 }
             }
         });
@@ -46,24 +72,65 @@ public class MainActivity extends AppCompatActivity {
         button.setLayoutParams(params);
     }
 
-    private Boolean atMiddleOfScreen(Button button) {
+    private Map<String, Integer> getButtonInfo(Button button) {
+        Map<String, Integer> buttonInfo = new HashMap<>();
+
+        // Get text
         String buttonText = button.getText().toString();
+        Log.d(TAG, "Button Text: " + buttonText);
 
-        if (buttonText.equals("Click To Start Link")) {
-           return Boolean.TRUE;
-        }
+        // Get layout parameters
+        ConstraintLayout.LayoutParams params = (ConstraintLayout.LayoutParams)
+                button.getLayoutParams();
+        Integer width = params.width;
+        Integer height = params.height;
 
-        return Boolean.FALSE;
+        buttonInfo.put("width", width);
+        buttonInfo.put("height", height);
+
+        Log.d(TAG, "Button Width: " + width + "px, Height: " + height + "px");
+
+        // Get position
+        int[] location = new int[2];
+        button.getLocationOnScreen(location);
+        Integer x = location[0];
+        Integer y = location[1];
+
+        buttonInfo.put("xPos", x);
+        buttonInfo.put("yPos", y);
+
+        Log.d(TAG, "Button Position: x=" + x + "px, y=" + y + "px");
+
+        return buttonInfo;
     }
 
-    private void changeButtonPropertiesToUnlinked(Button button) {
-        button.setText("Click To Start Link");
+    private void setButtonProperties(Button button, Map<String, Integer> map) {
+        Integer width = map.get("width");
+        Integer height = map.get("height");
+        Integer xPos = map.get("xPos");
+        Integer yPos = map.get("yPos");
 
-        LayoutParams params = new LayoutParams(200, 150);
-        params.setMargins(0, 16, 0, 0);
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(width, height);
+        params.setMargins(xPos, yPos, 0, 0);
 
         button.setLayoutParams(params);
     }
+
+//    private void setButtonPositionToMiddle(Button button) {
+//        ConstraintLayout constraintLayout = findViewById(R.id.constraintLayout);
+//
+//        ConstraintSet constraintSet = new ConstraintSet();
+//        constraintSet.clone(constraintLayout);
+//
+//        // Set constraints to center the button
+//        constraintSet.connect(myButton.getId(), ConstraintSet.TOP, ConstraintSet.PARENT_ID, ConstraintSet.TOP);
+//        constraintSet.connect(myButton.getId(), ConstraintSet.BOTTOM, ConstraintSet.PARENT_ID, ConstraintSet.BOTTOM);
+//        constraintSet.connect(myButton.getId(), ConstraintSet.START, ConstraintSet.PARENT_ID, ConstraintSet.START);
+//        constraintSet.connect(myButton.getId(), ConstraintSet.END, ConstraintSet.PARENT_ID, ConstraintSet.END);
+//
+//        // Apply the constraints
+//        constraintSet.applyTo(constraintLayout);
+//    }
 
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
