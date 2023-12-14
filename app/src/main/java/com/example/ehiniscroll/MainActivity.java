a9c336b44757344e514d56d9444ee105fd974f07
Stingray <stingraysonggay@gmail.com>
2023-12-14 11:06:00 +0800

    new views, formatting changes, function dec..
    - added edittext for ip input
    - new functions coding conventions

--- app/src/main/java/com/example/ehiniscroll/MainActivity.java
+++ app/src/main/java/com/example/ehiniscroll/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ScrollView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -24,24 +25,27 @@ public class MainActivity extends AppCompatActivity {
         ProgressBar linkingBar = findViewById(R.id.loadingProgressBar);
         ScrollView verticalScrollView = findViewById(R.id.verticalScrollView);
         TextView invisibleTextView = findViewById(R.id.invisibleTextView);
+        EditText ipAddressBox = findViewById(R.id.editText);
+        TextView connectedTextView = findViewById(R.id.connectedTextView);
 
         startButton.setOnClickListener(v -> {
+            String ipAddress = ipAddressBox.getText().toString();
             showToast("Initializing Link");
-            startButton.setVisibility(View.INVISIBLE);
-            linkingBar.setVisibility(View.VISIBLE);
-            stopButton.setVisibility(View.VISIBLE);
+            setViewToConnecting(startButton, linkingBar, stopButton);
             // wifi logic here
 
-            linkingBar.setVisibility(View.INVISIBLE);
-            System.out.println("Link Started");
-            invisibleTextView.setVisibility(View.VISIBLE);
+            setViewToConnected(
+                    ipAddressBox,
+                    connectedTextView,
+                    linkingBar,
+                    invisibleTextView,
+                    ipAddress);
         });
 
         stopButton.setOnClickListener(v -> {
             // wifi logic here
 
-            startButton.setVisibility(View.VISIBLE);
-            stopButton.setVisibility(View.INVISIBLE);
+            setViewsToMain(startButton, stopButton, connectedTextView, ipAddressBox);
 
             showToast("Link Stopped");
             System.out.println("Link Stopped");
@@ -54,7 +58,6 @@ public class MainActivity extends AppCompatActivity {
             }
 
             if (scrollY >= 22000) {
-                showToast("NEAR END");
                 verticalScrollView.fullScroll(ScrollView.FOCUS_UP);
             }
         });
@@ -64,5 +67,39 @@ public class MainActivity extends AppCompatActivity {
 
     private void showToast(String message) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
+    private void setViewsToMain(Button startButton,
+            Button stopButton,
+            TextView connectedTextView,
+            EditText ipAddressBox) {
+        startButton.setVisibility(View.VISIBLE);
+        ipAddressBox.setVisibility(View.VISIBLE);
+        stopButton.setVisibility(View.INVISIBLE);
+        connectedTextView.setVisibility(View.INVISIBLE);
+        connectedTextView.setText("");
+    }
+
+    private void setViewToConnecting(Button startButton,
+                                     ProgressBar linkingBar,
+                                     Button stopButton) {
+        startButton.setVisibility(View.INVISIBLE);
+        linkingBar.setVisibility(View.VISIBLE);
+        stopButton.setVisibility(View.VISIBLE);
+    }
+
+    private void setViewToConnected(EditText ipAddressBox,
+                                    TextView connectedTextView,
+                                    ProgressBar linkingBar,
+                                    TextView invisibleTextView,
+                                    String ipAddress) {
+        ipAddressBox.setVisibility(View.INVISIBLE);
+        connectedTextView.setText(
+                "Connected to " + ipAddress + "\n " +
+                "You can now scroll");
+        connectedTextView.setVisibility(View.VISIBLE);
+        linkingBar.setVisibility(View.INVISIBLE);
+        System.out.println("Link Started");
+        invisibleTextView.setVisibility(View.VISIBLE);
     }
 }
